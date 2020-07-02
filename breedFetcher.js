19ef83c3f6cb3dd0ab4f10a5abde2bea290dc235
Sara Irving <sarakirving@gmail.com>
2020-07-02 22:07:20 +0000

    Fetches breed information from thecatapi.com

---
+++ breedFetcher.js
@@ -0,0 +1,17 @@
+const request = require('request');
+const input = process.argv.slice(2).join();
+const URL = 'https://api.thecatapi.com/v1/breeds/search?q=' + input;
+
+request(URL, (error, response, body) => {
+
+  if (!error) {
+    const data = JSON.parse(body);
+    if (data[0]) {
+      console.log('The first entry is: ', data[0]);
+    } else {
+      console.log("The breed does not exist");
+    }
+  } else {
+    console.log("There was an error: ", error);
+  }
+});
