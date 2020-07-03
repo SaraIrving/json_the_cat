4423bbc2a2f80a59fc9f0bd0b51da7d6daded7e1
Sara Irving <sarakirving@gmail.com>
2020-07-03 00:14:29 +0000

    Removed commented out code

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,6 @@
 const request = require('request');
-//const input = process.argv.slice(2).join();
+
 const URL = 'https://api.thecatapi.com/v1/breeds/search?q=';
-
-// request(URL, (error, response, body) => {
-//   const data = JSON.parse(body);
-//   if (!error) {
-//     if (data[0]) {
-//       console.log('The first entry is: ', data[0]);
-//     } else {
-//       console.log("The breed does not exist");
-//     }
-//   } else {
-//     console.log("There was an error: ", error);
-//   }
-// });
-
 
 const fetchBreedDescription = function(breedName, callback) {
   request(URL + breedName, (error, response, body) => {
