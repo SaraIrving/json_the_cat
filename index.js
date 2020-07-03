ad58b9bc63c3431370f5c3fd62a9f47175b39ac7
Sara Irving <sarakirving@gmail.com>
2020-07-03 00:12:28 +0000

    Edited the fetchBreedDescription callback to handle an invalid breedname

---
+++ index.js
@@ -0,0 +1,17 @@
+// index.js
+const { fetchBreedDescription } = require('./breedFetcher');
+
+const breedName = process.argv[2];
+//const input = process.argv.slice(2).join();
+
+fetchBreedDescription(breedName, (error, data) => {
+  if (error) {
+    console.log('Error fetch details:', error);
+  } else {
+    if(data){
+      console.log(data.description);
+    } else {
+      console.log('Breed not found'); 
+    }
+  }
+});
